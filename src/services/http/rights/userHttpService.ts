7c433e7a3d2453123f64add04aa95a66340c0fef
William Sykes <williamsykesthe3rd@gmail.com>
2025-06-17 19:01:46 +0200

    removed unused register fields from copy

--- src/services/http/rights/userHttpService.ts
+++ src/services/http/rights/userHttpService.ts
@@ -11,12 +11,6 @@ export default class UserHttpService {
         form.append('Email', registration.email);
         form.append('Password', registration.password);
         form.append('Name', `${registration.name} ${registration.surname}`);
-        form.append('NameOfOrganisation', registration.nameOfOrganisation);
-        form.append('TypeOfOrganisation', `${registration.typeOfOrganisation}`);
-        form.append('TypeOfOrganisationOther', registration.typeOfOrganisationOther ?? '');
-        form.append('Sector', `${registration.sector}`);
-        form.append('SectorOther', registration.sectorOther ?? '');
-        form.append('RoleText', registration.roleText ?? '');
 
         // We use create here to make sure axios does not try and send an expired token or
         // any other headers not needed, basically to ignore the interceptors.
